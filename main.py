01fe2d8f5b517ec54bcb0adbf7603e5d05b3b560
TCKACHIKSIS <e79532157848@gmail.com>
2021-07-29 12:23:01 +1000

    запилил базу данных

--- main.py
+++ main.py
@@ -1,16 +1,69 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import sqlite3
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Student:
+    def __init__(self, id, name, last_name, student_class):
+        self.id = id
+        self.name = name
+        self.last_name = last_name
+        self.student_class = student_class
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Rating:
+    def __init__(self, id, math, phys, comp_science):
+        self.id = id
+        self.math = math
+        self.phys = phys
+        self.comp_science = comp_science
+        self.average_score = (math+phys+comp_science)/3
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def load_start_values():
+    student_list = [
+        Student(1, "Егор", "Ткаченко", "11 А"),
+        Student(2, "Вася", "Петров", "5 В"),
+        Student(3, "Алексей", "Петухов", "5 В"),
+        Student(4, "Павел", "Пехов", "11 А"),
+        Student(5, "Сергей", "Алексеев", "7 Б")
+    ]
+    rating_list = [
+        Rating(1, 5, 5, 5),
+        Rating(2, 4, 3, 5),
+        Rating(3, 4, 5, 5),
+        Rating(4, 2, 5, 3),
+        Rating(5, 3, 4, 4)
+    ]
+
+    for student in student_list:
+        cursor.execute("INSERT INTO students VALUES (?, ?, ?, ?);", [student.id, student.name, student.last_name, student.student_class])
+        connect.commit()
+
+    for rating in rating_list:
+        cursor.execute("INSERT INTO rating VALUES (?, ?, ?, ?, ?);", [rating.id, rating.math, rating.phys, rating.comp_science, rating.average_score])
+        connect.commit()
+
+
+connect = sqlite3.connect('students.db')
+cursor = connect.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS students(
+    student_id INTEGER,
+    student_name TEXT,
+    last_name TEXT,
+    student_class TEXT
+)
+""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS rating(
+    id INTEGER,
+    math INTEGER,
+    phys INTEGER ,
+    computer_science INTEGER ,
+    average_score FLOAT 
+)
+""")
+
+connect.commit()
+if len(list(cursor.execute("SELECT * FROM students"))) == 0:
+    load_start_values()
+
